925704eb7063af74ce3072c459d8811fa1f7e4a4
Eric Maupin <me@ermau.com>
2009-11-07 02:16:49 +0000

    Added MutableLookup<TKey,TElement> and tests

---
+++ Mono.Rocks/MutableLookup.cs
@@ -0,0 +1,202 @@
+//
+// MutableLookup.cs
+//
+// Author:
+//   Eric Maupin  <me@ermau.com>
+//
+// Copyright (c) 2009 Eric Maupin (http://www.ermau.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mono.Rocks
+{
+	/// <summary>
+	/// A mutable lookup implementing <see cref="ILookup{TKey,TElement}"/>
+	/// </summary>
+	/// <typeparam name="TKey">The lookup key.</typeparam>
+	/// <typeparam name="TElement">The elements under each <typeparamref name="TKey"/>.</typeparam>
+	public class MutableLookup<TKey, TElement> : ILookup<TKey, TElement>
+	{
+		public MutableLookup ()
+		{
+			this.groupings = new Dictionary<TKey, MutableLookupGrouping> ();
+			if (!typeof(TKey).IsValueType)
+				this.nullGrouping = new MutableLookupGrouping (default(TKey));
+		}
+
+		public MutableLookup (ILookup<TKey, TElement> lookup)
+		{
+			if (lookup == null)
+				throw new ArgumentNullException ("lookup");
+			
+			this.groupings = new Dictionary<TKey, MutableLookupGrouping> (lookup.Count);
+			
+			if (!typeof(TKey).IsValueType)
+				this.nullGrouping = new MutableLookupGrouping (default(TKey), lookup[default(TKey)]);
+			
+			foreach (var grouping in lookup) {
+				if (grouping.Key == null)
+					continue;
+				
+				this.groupings.Add (grouping.Key, new MutableLookupGrouping (grouping.Key, grouping));
+			}
+		}
+
+		/// <summary>
+		/// Adds <paramref name="element"/> under the specified <paramref name="key"/>. <paramref name="key"/> does not need to exist.
+		/// </summary>
+		/// <param name="key">The key to add <paramref name="element"/> under.</param>
+		/// <param name="element">The element to add.</param>
+		public void Add (TKey key, TElement element)
+		{
+			if (key == null) {
+				this.nullGrouping.Add (element);
+				return;
+			}
+			
+			if (!this.groupings.ContainsKey (key))
+				this.groupings.Add (key, new MutableLookupGrouping (key));
+			
+			this.groupings[key].Add (element);
+		}
+
+		/// <summary>
+		/// Removes <paramref name="element"/> from the <paramref name="key"/>.
+		/// </summary>
+		/// <param name="key">The key that <paramref name="element"/> is located under.</param>
+		/// <param name="element">The element to remove from <paramref name="key"/>. </param>
+		/// <returns><c>true</c> if <paramref name="key"/> and <paramref name="element"/> existed, <c>false</c> if not.</returns>
+		public bool Remove (TKey key, TElement element)
+		{
+			if (key == null)
+				return this.nullGrouping.Remove (element);
+			
+			if (!this.groupings.ContainsKey (key))
+				return false;
+			
+			if (this.groupings[key].Remove (element)) {
+				if (this.groupings[key].Count == 0)
+					this.groupings.Remove (key);
+				
+				return true;
+			}
+			
+			return false;
+		}
+
+		/// <summary>
+		/// Removes <paramref name="key"/> from the lookup.
+		/// </summary>
+		/// <param name="key">They to remove.</param>
+		/// <returns><c>true</c> if <paramref name="key"/> existed.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="key"/> is <c>null</c>.</exception>
+		public bool Remove (TKey key)
+		{
+			if (key == null) {
+				bool removed = (this.nullGrouping.Count > 0);
+				this.nullGrouping.Clear ();
+				return removed;
+			}
+			
+			return this.groupings.Remove (key);
+		}
+
+		#region ILookup Members
+		/// <summary>
+		/// Gets the number of groupings.
+		/// </summary>
+		public int Count {
+			get { return this.groupings.Count + ((this.nullGrouping != null && this.nullGrouping.Count > 0) ? 1 : 0); }
+		}
+
+		/// <summary>
+		/// Gets the elements for <paramref name="key"/>.
+		/// </summary>
+		/// <param name="key">The key to get the elements for.</param>
+		/// <returns>The elements under <paramref name="key"/>.</returns>
+		public IEnumerable<TElement> this[TKey key] {
+			get {
+				if (key == null)
+					return this.nullGrouping;
+				
+				MutableLookupGrouping grouping;
+				if (this.groupings.TryGetValue (key, out grouping))
+					return grouping;
+				
+				return new TElement[0];
+			}
+		}
+
+		/// <summary>
+		/// Gets whether or not there's a grouping for <paramref name="key"/>.
+		/// </summary>
+		/// <param name="key">The key to check for.</param>
+		/// <returns><c>true</c> if <paramref name="key"/> is present.</returns>
+		public bool Contains (TKey key)
+		{
+			if (key == null)
+				return (this.nullGrouping.Count > 0);
+			
+			return this.groupings.ContainsKey (key);
+		}
+
+		public IEnumerator<IGrouping<TKey, TElement>> GetEnumerator ()
+		{
+			foreach (var g in this.groupings.Values)
+				yield return g;
+			
+			if (this.nullGrouping != null)
+				yield return this.nullGrouping;
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
+		{
+			return this.GetEnumerator ();
+		}
+		#endregion
+
+		private readonly Dictionary<TKey, MutableLookupGrouping> groupings;
+		private readonly MutableLookupGrouping nullGrouping;
+
+		private class MutableLookupGrouping : List<TElement>, IGrouping<TKey, TElement>
+		{
+			public MutableLookupGrouping (TKey key)
+			{
+				this.Key = key;
+			}
+
+			public MutableLookupGrouping (TKey key, IEnumerable<TElement> collection) : base(collection)
+			{
+				this.Key = key;
+			}
+
+			#region IGrouping<TKey,TElement> Members
+
+			public TKey Key { get; private set; }
+
+			#endregion
+		}
+	}
+}
---
+++ Tests/Mono.Rocks.Tests/MutableLookupTest.cs
@@ -0,0 +1,313 @@
+//
+// MutableLookupTest.cs
+//
+// Author:
+//   Eric Maupin  <me@ermau.com>
+//
+// Copyright (c) 2009 Eric Maupin (http://www.ermau.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Mono.Rocks.Tests
+{
+	[TestFixture]
+	public class MutableLookupTest
+	{
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void CtorNull()
+		{
+			new MutableLookup<string, string> (null);
+		}
+
+		[Test]
+		public void CtorILookup()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup <int, int> (ints.ToLookup (i => Int32.Parse (i.ToString()[0].ToString())));
+
+			Assert.AreEqual (3, lookup.Count);
+			Assert.AreEqual (1, lookup[1].Count());
+			Assert.AreEqual (2, lookup[2].Count());
+			Assert.AreEqual (1, lookup[3].Count());
+		}
+
+		[Test]
+		public void CtorILookupWithNulls()
+		{
+			List<string> strs = new List<string> { "Foo", "Foos", "Foobar", "Monkeys", "Bar", "Ban", "Barfoo" };
+
+			var lookup = new MutableLookup<string, string> (strs.ToLookup (s => (s[0] != 'F' && s[0] != 'B') ? null : s[0].ToString()));
+			Assert.AreEqual (3, lookup.Count);
+			Assert.AreEqual (3, lookup["F"].Count());
+			Assert.AreEqual (3, lookup["B"].Count());
+			Assert.AreEqual (1, lookup[null].Count());
+		}
+
+		[Test]
+		public void Add()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add ("F", "Foo");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual ("Foo", lookup["F"].First());
+			lookup.Add ("F", "Foobar");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (2, lookup["F"].Count());
+		}
+
+		[Test]
+		public void AddNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "Foo");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (1, lookup[null].Count());
+			lookup.Add (null, null);
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (2, lookup[null].Count());
+		}
+
+		[Test]
+		public void CountRefType()
+		{
+			var lookup = new MutableLookup<string, string>();
+			Assert.AreEqual (0, lookup.Count);
+			lookup.Add (null, "blah");
+			Assert.AreEqual (1, lookup.Count);
+			lookup.Add ("F", "Foo");
+			Assert.AreEqual (2, lookup.Count);
+			lookup.Add ("F", "Foobar");
+			Assert.AreEqual (2, lookup.Count);
+
+			lookup.Remove (null, "blah");
+			Assert.AreEqual (1, lookup.Count);
+		}
+
+		[Test]
+		public void CountValueType()
+		{
+			var lookup = new MutableLookup<int, int>();
+			Assert.AreEqual (0, lookup.Count);
+			lookup.Add (1, 10);
+			Assert.AreEqual (1, lookup.Count);
+			lookup.Add (2, 20);
+			Assert.AreEqual (2, lookup.Count);
+			lookup.Add (2, 21);
+			Assert.AreEqual (2, lookup.Count);
+
+			lookup.Remove (1, 10);
+			Assert.AreEqual(1, lookup.Count);
+		}
+
+		[Test]
+		public void RemoveExisting()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			Assert.AreEqual (3, lookup.Count);
+
+			Assert.IsTrue (lookup.Remove (null, "blah"));
+			Assert.AreEqual (3, lookup.Count);
+			Assert.IsTrue (lookup.Remove (null, "monkeys"));
+			Assert.AreEqual (2, lookup.Count);
+
+			Assert.IsTrue (lookup.Remove ("F"));
+			Assert.AreEqual (1, lookup.Count);
+		}
+
+		[Test]
+		public void RemoveNonExisting()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Remove ("D"));
+			Assert.AreEqual(3, lookup.Count);
+
+			Assert.IsFalse (lookup.Remove ("F", "asdf"));
+			Assert.AreEqual(3, lookup.Count);
+
+			lookup.Remove (null);
+			Assert.IsFalse (lookup.Remove (null));
+			Assert.AreEqual (2, lookup.Count);
+		}
+
+		[Test]
+		public void Contains()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsTrue (lookup.Contains ("B"));
+		}
+
+		[Test]
+		public void DoesNotContain()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Contains ("D"));
+		}
+
+		[Test]
+		public void ContainsNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsTrue (lookup.Contains (null));
+		}
+
+		[Test]
+		public void DoesNotContainNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Contains (null));
+		}
+
+		[Test]
+		public void Indexer()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.AreEqual (2, lookup["F"].Count());
+		}
+
+		[Test]
+		public void IndexerNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.AreEqual (2, lookup[null].Count());
+		}
+
+		[Test]
+		public void IndexerNotFound()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			Assert.AreEqual (0, lookup["D"].Count());
+		}
+
+		[Test]
+		public void Enumerator()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup<int, int>(ints.ToLookup(i => Int32.Parse(i.ToString()[0].ToString())));
+
+			Assert.AreEqual (3, lookup.Count());
+			Assert.IsTrue (lookup.Any (g => g.Key == 1));
+			Assert.IsTrue (lookup.Any (g => g.Key == 2));
+			Assert.IsTrue (lookup.Any (g => g.Key == 3));
+		}
+
+		[Test]
+		public void EnumeratorNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			Assert.AreEqual (3, lookup.Count());
+			Assert.IsTrue (lookup.Any (g => g.Key == null));
+			Assert.IsTrue (lookup.Any (g => g.Key == "F"));
+			Assert.IsTrue (lookup.Any (g => g.Key == "B"));
+		}
+
+		[Test]
+		public void NullGroupingEnumerator()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, null);
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			Assert.AreEqual (3, lookup[null].Count());
+			Assert.IsTrue (lookup[null].Any (s => s == "blah"));
+			Assert.IsTrue (lookup[null].Any (s => s == "monkeys"));
+			Assert.IsTrue (lookup[null].Any (s => s == null));
+		}
+
+		[Test]
+		public void GroupingEnumerator()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup<int, int>(ints.ToLookup(i => Int32.Parse(i.ToString()[0].ToString())));
+
+			Assert.AreEqual (2, lookup[2].Count());
+			Assert.IsTrue (lookup[2].Any (i => i == 20));
+			Assert.IsTrue (lookup[2].Any(i => i == 21));
+		}
+	}
+}
