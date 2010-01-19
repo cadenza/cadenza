9ca5427297b14c2c5bae871eaf225fd5ce092c30
Chris Howie <cdhowie@gmail.com>
2010-01-19 17:58:00 -0500

    Ensure that ObservableHashSet<T> has the same methods as HashSet<T>

--- Cdh.Toolkit.Collections/Observable/ObservableHashSet.cs
+++ Cdh.Toolkit.Collections/Observable/ObservableHashSet.cs
@@ -6,6 +6,7 @@ using System.Threading;
 
 using Cdh.Toolkit.Extensions.Events;
 using Cdh.Toolkit.Extensions.ReaderWriterLockSlim;
+using System.Security;
 
 namespace Cdh.Toolkit.Collections.Observable
 {
@@ -27,12 +28,16 @@ namespace Cdh.Toolkit.Collections.Observable
 
         protected void FireAdded(T item)
         {
-            Changed.Fire(this, new ObservableCollectionChangedEventArgs<T>(item, ObservableChangeType.Add));
+            var handler = Changed;
+            if (handler != null)
+                handler(this, new ObservableCollectionChangedEventArgs<T>(item, ObservableChangeType.Add));
         }
 
         protected void FireRemoved(T item)
         {
-            Changed.Fire(this, new ObservableCollectionChangedEventArgs<T>(item, ObservableChangeType.Remove));
+            var handler = Changed;
+            if (handler != null)
+                handler(this, new ObservableCollectionChangedEventArgs<T>(item, ObservableChangeType.Remove));
         }
 
         public override void Add(T item)
@@ -71,6 +76,197 @@ namespace Cdh.Toolkit.Collections.Observable
         #region IObservableCollection<T> Members
 
         public event EventHandler<ObservableCollectionChangedEventArgs<T>> Changed;
+
+        #endregion
+
+        private T[] CloneAsArray()
+        {
+            T[] clone = new T[Decorated.Count];
+            Decorated.CopyTo(clone);
+
+            return clone;
+        }
+
+        #region HashSet<T> decorations
+
+        public virtual void CopyTo(T[] array)
+        {
+            using (Lock.Read())
+                Decorated.CopyTo(array);
+        }
+
+        public virtual void CopyTo(T[] array, int arrayIndex, int count)
+        {
+            using (Lock.Read())
+                Decorated.CopyTo(array, arrayIndex, count);
+        }
+
+        public virtual void ExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Write())
+                foreach (T item in other)
+                    if (Decorated.Remove(item))
+                        FireRemoved(item);
+        }
+
+        public virtual void IntersectWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Write())
+            {
+                T[] clone = CloneAsArray();
+
+                foreach (T item in clone)
+                    if (!other.Contains(item))
+                    {
+                        Decorated.Remove(item);
+                        FireRemoved(item);
+                    }
+
+                foreach (T item in other)
+                    if (!Decorated.Contains(item))
+                    {
+                        Decorated.Remove(item);
+                        FireRemoved(item);
+                    }
+            }
+        }
+
+        public virtual bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.IsProperSubsetOf(other);
+        }
+
+        public virtual bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.IsProperSupersetOf(other);
+        }
+
+        public virtual bool IsSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.IsSubsetOf(other);
+        }
+
+        public virtual bool IsSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.IsSupersetOf(other);
+        }
+
+        public virtual bool Overlaps(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.Overlaps(other);
+        }
+
+        public virtual int RemoveWhere(Predicate<T> match)
+        {
+            if (match == null)
+                throw new ArgumentNullException("match");
+
+            int count = 0;
+
+            using (Lock.Write())
+            {
+                T[] clone = CloneAsArray();
+
+                foreach (T item in clone)
+                {
+                    if (match(item))
+                    {
+                        Decorated.Remove(item);
+                        FireRemoved(item);
+
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public virtual bool SetEquals(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Read())
+                return Decorated.SetEquals(other);
+        }
+
+        private IEnumerable<T> MakeDistinct(IEnumerable<T> other)
+        {
+            HashSet<T> otherSet = other as HashSet<T>;
+            if (otherSet != null && otherSet.Comparer.Equals(Decorated.Comparer))
+                return other;
+
+            return other.Distinct(Decorated.Comparer);
+        }
+
+        public virtual void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Write())
+            {
+                foreach (T item in MakeDistinct(other))
+                {
+                    if (Decorated.Contains(item))
+                    {
+                        Decorated.Remove(item);
+                        FireRemoved(item);
+                    }
+                    else
+                    {
+                        Decorated.Add(item);
+                        FireAdded(item);
+                    }
+                }
+            }
+        }
+
+        public virtual void TrimExcess()
+        {
+            using (Lock.Write())
+                Decorated.TrimExcess();
+        }
+
+        public virtual void UnionWith(IEnumerable<T> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException("other");
+
+            using (Lock.Write())
+            {
+                foreach (T item in other)
+                    if (Decorated.Add(item))
+                        FireAdded(item);
+            }
+        }
 
         #endregion
     }
