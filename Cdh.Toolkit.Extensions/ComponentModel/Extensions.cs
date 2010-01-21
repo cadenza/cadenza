aa0f28da9f151df32776a48b6ad8cd62182d72c6
Chris Howie <cdhowie@gmail.com>
2010-01-21 15:18:09 -0500

    Add ComponentModel extensions

---
+++ Cdh.Toolkit.Extensions/ComponentModel/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Cdh.Toolkit.Extensions.ComponentModel
+{
+    public static class Extensions
+    {
+        public static void AutoInvoke(this ISynchronizeInvoke obj, Action method)
+        {
+            if (obj.InvokeRequired)
+                obj.Invoke(method, null);
+            else
+                method();
+        }
+
+        public static object AutoInvoke(this ISynchronizeInvoke obj, Delegate method, params object[] args)
+        {
+            if (obj.InvokeRequired)
+                return obj.Invoke(method, args);
+            else
+                return method.Method.Invoke(method.Target, args);
+        }
+
+        public static AsyncCallback Invoked(this AsyncCallback callback, ISynchronizeInvoke obj)
+        {
+            return result => obj.AutoInvoke(callback, result);
+        }
+    }
+}
