f7eb1c2b0fe26ca0015754b6260925fb597a3042
Jonathan Pryor <jonpryor@vt.edu>
2010-03-02 14:37:43 -0500

    Begin providing a generic math infrastructure.
    The eventual goal is twofold:
    
     1. Provide a way to perform mathematical operations on generic types.
        In this case, MathProvider<int>.Default.Add(1, 2) == 1+2.
    
     2. Eventually, use (1) in order to provide math-based extension methods,
        such as an EnumerableCoda.StandardDeviations() method.
    
    This is currently just a transcoding of the basic Haskell interfaces into C#.

---
+++ src/Cadenza.Constraints/Cadenza.Numerics/ExpressionMathProvider.cs
@@ -0,0 +1,74 @@
+//
+// ExpressionMathProvider.cs
+//
+// Author:
+//   Jonathan Pryor <jpryor@novell.com>
+//
+// Copyright (c) 2010 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using Cadenza;
+
+namespace Cadenza.Numerics {
+
+	// from: http://www.yoda.arachsys.com/csharp/genericoperators.html
+	public class ExpressionMathProvider<T> : MathProvider<T>
+	{
+		static Func<T, T, T> add    = CreateBinaryExpression<T> ((a, b) => Expression.Add (a, b));
+		static Func<T, T, T> sub    = CreateBinaryExpression<T> ((a, b) =>Expression.SubtractChecked (a, b));
+		static Func<T, T, T> divide = CreateBinaryExpression<T> ((a, b) => Expression.Divide (a, b));
+		static Func<T, T, T> mod    = CreateBinaryExpression<T> ((a, b) => Expression.Modulo (a, b));
+		static Func<T, T, T> mult   = CreateBinaryExpression<T> ((a, b) => Expression.MultiplyChecked (a, b));
+		static Func<T, T, T> pow    = CreateBinaryExpression<T>((a, b) => Expression.Power (a, b));
+		static Func<T, T> negate    = CreateUnaryExpression (v => Expression.NegateChecked (v));
+		static Func<T, T, bool> eq  = CreateBinaryExpression<bool> ((a, b) => Expression.Equal (a, b));
+		static Func<T, T, bool> gt  = CreateBinaryExpression<bool> ((a, b) => Expression.GreaterThan (a, b));
+		static Func<T, T, bool> gte = CreateBinaryExpression<bool> ((a, b) => Expression.GreaterThanOrEqual (a, b));
+		static Func<T, T, bool> lt  = CreateBinaryExpression<bool> ((a, b) => Expression.LessThan (a, b));
+		static Func<T, T, bool> lte = CreateBinaryExpression<bool> ((a, b) => Expression.LessThanOrEqual (a, b));
+
+		static Func<T, T, TRet> CreateBinaryExpression<TRet> (Func<ParameterExpression, ParameterExpression, BinaryExpression> operation)
+		{
+			var a = Expression.Parameter (typeof (T), "a");
+			var b = Expression.Parameter (typeof (T), "b");
+			var body = operation (a, b);
+			return Expression.Lambda<Func<T, T, TRet>> (body, a, b).Compile ();
+		}
+
+		static Func<T, T> CreateUnaryExpression (Func<ParameterExpression, UnaryExpression> operation)
+		{
+			var value = Expression.Parameter (typeof (T), "value");
+			var body = operation (value);
+			return Expression.Lambda<Func<T, T>> (body, value).Compile ();
+		}
+
+		public T Add (T x, T y)
+		{
+			return add (x, y);
+		}
+	}
+}
---
+++ src/Cadenza/Cadenza.Numerics/MathProvider.cs
@@ -0,0 +1,276 @@
+//
+// MathProvider.cs
+//
+// Author:
+//   Jonathan Pryor <jpryor@novell.com>
+//
+// Copyright (c) 2010 Novell, Inc. (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cadenza;
+
+namespace Cadenza.Numerics {
+
+	//
+	// Interfaced based on Haskell data type interfaces from:
+	//    http://www.haskell.org/ghc/docs/latest/html/libraries/base-4.2.0.0/Prelude.html
+	//
+
+	public interface IOrderedTypeProvider<T> : IComparer<T>
+	{
+		#region class Eq a => Ord a where
+		// compare is in IComparer<T>
+		bool LessThan (T x, T y);
+		bool LessThanOrEqual (T x, T y);
+		bool GreaterThan (T x, T y);
+		bool GreaterThanOrEqual (T x, T y);
+		T Max (T x, T y);
+		T Min (T x, T y);
+		#endregion
+	}
+
+	public interface ISequentiallyOrderedTypeProvider<T>
+	{
+		#region class Enum a where
+		T Successor (T value);
+		T Predecessor (T value);
+		T Parse (int value);
+		int ToInt32 (T value);
+		IEnumerable<T> EnumerateFrom (T value);
+		IEnumerable<T> EnumerateFromThen (T first, T start);
+		IEnumerable<T> EnumerateFromTo (T start, T end);
+		IEnumerable<T> EnumerateFromThenTo (T first, T start, T end);
+		#endregion
+	}
+
+	public static class SequentiallyOrderedTypeProviderCoda
+	{
+		// Default implmentations
+		public static T DefaultSuccessor<T> (ISequentiallyOrderedTypeProvider<T> self, T value)
+		{
+			Check.Self (self);
+
+			return self.Parse (checked (self.ToInt32 (value) + 1));
+		}
+
+		public static T DefaultPredecessor<T> (ISequentiallyOrderedTypeProvider<T> self, T value)
+		{
+			Check.Self (self);
+
+			return self.Parse (checked (self.ToInt32 (value) - 1));
+		}
+
+		public static IEnumerable<T> DefaultEnumFrom<T> (ISequentiallyOrderedTypeProvider<T> self, T value)
+		{
+			Check.Self (self);
+
+			return Sequence.Iterate (self.ToInt32 (value), v => checked (v + 1))
+				.Select (v => self.Parse (v));
+		}
+
+		public static IEnumerable<T> DefaultEnumFromThen<T> (ISequentiallyOrderedTypeProvider<T> self, T first, T start)
+		{
+			Check.Self (self);
+
+			return new[]{self.ToInt32 (first)}
+				.Concat (Sequence.Iterate (self.ToInt32 (start), v => checked (v + 1)))
+				.Select (v => self.Parse (v));
+		}
+
+		public static IEnumerable<T> DefaultEnumFromTo<T> (ISequentiallyOrderedTypeProvider<T> self, T start, T end)
+		{
+			Check.Self (self);
+
+			int s = self.ToInt32 (start);
+			int e = self.ToInt32 (end);
+
+			return Enumerable.Range (s, e - s).Select (v => self.Parse (v));
+		}
+
+		public static IEnumerable<T> DefaultEnumFromThenTo<T> (ISequentiallyOrderedTypeProvider<T> self, T first, T start, T end)
+		{
+			Check.Self (self);
+
+			int s = self.ToInt32 (start);
+			int e = self.ToInt32 (end);
+
+			return new[]{first}.Concat (Enumerable.Range (s, e - s).Select (v => self.Parse (v)));
+		}
+	}
+
+	public interface IBoundedProvider<T>
+	{
+		#region class Bounded a where
+		T MinBound ();
+		T MaxBound ();
+		#endregion
+	}
+
+	// TODO: support Rational?
+	// TODO: how should we support Integer?  it's a variable-sized integer type
+
+	public interface INumericProvider<T> : IEqualityComparer<T>
+	{
+		#region class (Eq a, Show a) => Num a where
+		T Add (T x, T y);
+		T Multiply (T x, T y);
+		T Subtract (T x, T y);
+		T Negate (T x);
+		T Abs (T value);
+		int Sign (T value);
+
+		T Parse (int value);
+		#endregion
+	}
+
+	public interface IRealProvider<T> : INumericProvider<T>, IOrderedTypeProvider<T>
+	{
+		// TODO? toRational :: a -> Rational
+	}
+
+	public interface IIntegralNumberProvider<T> : IRealProvider<T>, ISequentiallyOrderedTypeProvider<T>
+	{
+		#region class (Real a, Enum a) => Integral a where
+		T Quotient (T x, T y);  // integer division truncating toward 0
+		T Remainder (T x, T y); // integer remainder; satisfies: (x `quot` y)*y + (x `rem` y) == x
+		T Divide (T x, T y);     // integer division truncated toward negative infinity
+		T Modulus (T x, T y);   // integer moduleus, satisfying: (x `div` y)*y + (x `mod` y) == x
+		Tuple<T, T> QuotientRemainder (T x, T y);
+		Tuple<T, T> DivideModulus (T x, T y);
+		long ToInt64 (T value); // toInteger
+		#endregion
+	}
+
+	public interface IFractionalProvider<T> : INumericProvider<T>
+	{
+		#region class Num a => Fractional a where
+		T Divide (T x, T y);
+		T Reciprocal (T value);
+		// TODO: fromRational
+		#endregion
+	}
+
+	public interface IFloatingProvider<T> : IFractionalProvider<T>
+	{
+		#region class Fractional a => Floating a where
+		T Pi {get;}
+		T E {get;}
+		T Exp (T value);
+		T Sqrt (T value);
+		T Log (T value);
+		T Exponentiate (T value, T exp);
+		T Log (T value, T newBase);
+		T Sin (T value);
+		T Tan (T value);
+		T Cos (T value);
+		T Asin (T value);
+		T Atan (T value);
+		T Acos (T value);
+		T Sinh (T value);
+		T Tanh (T value);
+		T Cosh (T value);
+		// TODO: asinh, atanh, acosh
+		#endregion
+	}
+
+	public interface IRealFracProvider<T> : IRealProvider<T>, IFractionalProvider<T>
+	{
+		#region class (Real a, Fractional a) => RealFrac a where
+		// TODO: properFraction :: Integral b => a ->(b, a)
+		// TODO: Haskell introduces an 'Integral b' parameter; don't understand its use.
+		T Truncate (T value);
+		T Round (T value);
+		T Ceiling (T value);
+		T Floor (T value);
+		#endregion
+	}
+
+	public interface IRealFloatProvider<T> : IRealFracProvider<T>, IFloatingProvider<T>
+	{
+		#region class (RealFrac a, Floating a) => RealFloat a where
+		int Radix (T value);
+		int Digits (T value);
+		Tuple<int, int> Range (T value);
+		// TODO: decodeFloat, encodeFloat, exponent, significand, scaleFloat
+		bool IsNaN (T value);
+		bool IsInfinite (T value);
+		// TODO: isDenormalized, isNegativeZero
+		bool IsIEEE (T value);
+		T Atan2 (T y, T x);
+		#endregion
+	}
+
+	public abstract partial class MathProvider<T> : IComparer<T>, IEqualityComparer<T> {
+
+		protected MathProvider ()
+		{
+		}
+
+		static MathProvider ()
+		{
+			// set defaultProvider...
+		}
+
+		static MathProvider<T> defaultProvider;
+		public MathProvider<T> Default {
+			get {return defaultProvider;}
+		}
+
+		public static void SetDefault (MathProvider<T> provider)
+		{
+			defaultProvider = provider;
+		}
+
+		#region IComparer<T>
+		public virtual int Compare (T x, T y)
+		{
+			return Comparer<T>.Default.Compare (x, y);
+		}
+		#endregion
+
+		#region IEqualityComparer<T>
+		public virtual bool Equals (T x, T y)
+		{
+			return EqualityComparer<T>.Default.Equals (x, y);
+		}
+
+		public virtual int GetHashCode (T obj)
+		{
+			return EqualityComparer<T>.Default.GetHashCode (obj);
+		}
+		#endregion
+
+		#region Numeric functions
+		// subtract?
+		#if false
+		public abstract bool Even (T value);
+		public abstract bool Odd (T value);
+		public abstract T LeastCommonMultiple (T a, T b);
+		#endif
+		// TODO: ^, ^^, fromIntegral, realToFrac
+		#endregion
+	}
+}
