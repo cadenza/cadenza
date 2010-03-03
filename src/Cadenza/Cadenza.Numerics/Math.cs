07fc92a7ab9f4ce961f04f8e38f446609dad7c49
Jonathan Pryor <jonpryor@vt.edu>
2010-03-03 17:19:33 -0500

    Code formatting changes.
    It's prettier when things line up.

--- src/Cadenza/Cadenza.Numerics/Math.cs
+++ src/Cadenza/Cadenza.Numerics/Math.cs
@@ -447,8 +447,8 @@ namespace Cadenza.Numerics {
 		public override int   Successor           (int value)     {return checked (value+1);}
 		public override int   Predecessor         (int value)     {return checked (value-1);}
 		public override int   FromInt32           (int value)     {return value;}
-		public override int   MinValue                           {get {return int.MinValue;}}
-		public override int   MaxValue                           {get {return int.MaxValue;}}
+		public override int   MinValue                            {get {return int.MinValue;}}
+		public override int   MaxValue                            {get {return int.MaxValue;}}
 		public override int   Add                 (int x, int y)  {return checked (x + y);}
 		public override int   Multiply            (int x, int y)  {return checked (x * y);}
 		public override int   Subtract            (int x, int y)  {return checked (x - y);}
@@ -464,6 +464,6 @@ namespace Cadenza.Numerics {
 		                        QuotientRemainder (int x, int y)  {return Tuple.Create (Quotient (x, y), Remainder (x, y));}
 		public override Tuple<int, int>
 		                        DivideModulus     (int x, int y)  {return Tuple.Create (Divide (x, y), Modulus (x, y));}
-		public override int   Reciprocal        (int value)       {NotZero (value); return 0;}
+		public override int   Reciprocal          (int value)     {NotZero (value); return 0;}
 	}
 }
