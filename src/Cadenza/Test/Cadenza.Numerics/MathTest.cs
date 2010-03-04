53ec308291ec564e5c75e7a46597a80ce8359a4b
Jonathan Pryor <jonpryor@vt.edu>
2010-03-04 17:10:31 -0500

    Add "minimal Math<T> implementation" tests.
    The question: how many methods must be implemented to get Math<T> support?
    
    The answer: as few as 4, or 5, or 6, depending on how you implement support
    for converting T to and from System.Int32.
    
    See the SimpleNumber(X)Math implementations (which implement Math<T> support
    for SimpleNumber(X)).

--- src/Cadenza/Test/Cadenza.Numerics/MathTest.cs
+++ src/Cadenza/Test/Cadenza.Numerics/MathTest.cs
@@ -27,6 +27,9 @@
 //
 
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 using NUnit.Framework;
 
@@ -34,7 +37,306 @@ using Cadenza;
 
 namespace Cadenza.Numerics.Tests {
 
+	//
+	// Builtin type tests
+	//
+
 	[TestFixture]
 	public class Int32MathTests : MathContract<int> {
+	}
+
+	//
+	// To implement Math<T> support for a new type, you obviously need a type.
+	// We also require the ability to create instances of that type from an int.
+	// There are three ways to do this:
+	//  1.  Override Math<T>.FromInt32() and Math<T>.ToInt32().
+	//  2.  Provide a System.CodeDom.TypeConverter for the type and annotate the
+	//      type with [TypeConverter]
+	//  3.  Implement System.IConvertible, and override Math<T>.FromInt32().
+	//      This is necessary because System.Int32 doesn't support conversion to
+	//      to arbitrary types.
+	//
+	// The type must also implement IComparer<T> and IEquatable<T>.
+	//
+	// SimpleNumber + SimpleNumberMath show approach (1).
+	// SimpleNumber2 + SimpleNumber2Converter + SimpleNumber2Math show approach (2).
+	// SimpleNumber3 + SimpleNumber3Math show approach (3).
+	//
+
+	public class SimpleNumber : IComparable<SimpleNumber>, IEquatable<SimpleNumber>
+	{
+		public SimpleNumber (int value)
+		{
+			Value = value;
+		}
+
+		public int CompareTo (SimpleNumber other)
+		{
+			if (other == null)
+				return -1;
+			return Value.CompareTo (other.Value);
+		}
+
+		public override int GetHashCode ()
+		{
+			return Value.GetHashCode ();
+		}
+
+		public override bool Equals (object obj)
+		{
+			return Equals (obj as SimpleNumber);
+		}
+
+		public bool Equals (SimpleNumber other)
+		{
+			if (other == null)
+				return false;
+			return Value.Equals (other.Value);
+		}
+
+		public int Value {get; private set;}
+	}
+
+	// minimal implementation
+	public class SimpleNumberMath : Math<SimpleNumber> {
+		public override SimpleNumber FromInt32 (int value)
+		{
+			return new SimpleNumber (value);
+		}
+
+		public override int ToInt32 (SimpleNumber value)
+		{
+			return value.Value;
+		}
+
+		public override SimpleNumber Add (SimpleNumber x, SimpleNumber y)
+		{
+			return new SimpleNumber (x.Value + y.Value);
+		}
+
+		public override SimpleNumber Subtract (SimpleNumber x, SimpleNumber y)
+		{
+			return new SimpleNumber (x.Value - y.Value);
+		}
+
+		public override SimpleNumber Multiply (SimpleNumber x, SimpleNumber y)
+		{
+			return new SimpleNumber (x.Value * y.Value);
+		}
+
+		public override SimpleNumber QuotientRemainder (SimpleNumber x, SimpleNumber y, out SimpleNumber remainder)
+		{
+			remainder = new SimpleNumber (x.Value % y.Value);
+			return new SimpleNumber (x.Value / y.Value);
+		}
+	}
+
+	[TestFixture]
+	public class SimpleNumberMathTests : MathContract<SimpleNumber> {
+
+		static SimpleNumberMathTests ()
+		{
+			Math<SimpleNumber>.SetDefault (new SimpleNumberMath ());
+		}
+	}
+
+	[TypeConverter (typeof (SimpleNumber2Converter))]
+	public class SimpleNumber2 : SimpleNumber, IComparable<SimpleNumber2>, IEquatable<SimpleNumber2>
+	{
+		public SimpleNumber2 (int value)
+			: base (value)
+		{
+		}
+
+		public int CompareTo (SimpleNumber2 x)
+		{
+			return base.CompareTo (x);
+		}
+
+		public bool Equals (SimpleNumber2 other)
+		{
+			return base.Equals (other);
+		}
+	}
+
+	public class SimpleNumber2Converter : TypeConverter {
+
+		KeyValuePair<Type, Func<object, SimpleNumber2>>[] convertFrom = new KeyValuePair<Type, Func<object, SimpleNumber2>>[]{
+			new KeyValuePair<Type, Func<object, SimpleNumber2>>(typeof (int), v => {
+				int? value = v as int?;
+				if (value.HasValue)
+					return new SimpleNumber2 (value.Value);
+				return null;
+			}),
+			new KeyValuePair<Type, Func<object, SimpleNumber2>>(typeof (int), v => {
+				string value = v as string;
+				if (value != null)
+					return new SimpleNumber2 (int.Parse (value));
+				return null;
+			}),
+		};
+
+		KeyValuePair<Type, Func<SimpleNumber2, object>>[] convertTo = new KeyValuePair<Type, Func<SimpleNumber2, object>>[]{
+			new KeyValuePair<Type, Func<SimpleNumber2, object>>(typeof (int), v => v.Value),
+			new KeyValuePair<Type, Func<SimpleNumber2, object>>(typeof (int), v => v.Value.ToString ()),
+		};
+
+		public override bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
+		{
+			if (convertFrom.Any (c => c.Key == sourceType))
+				return true;
+			return base.CanConvertFrom (context, sourceType);
+		}
+
+		public override object ConvertFrom (ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+		{
+			foreach (var c in convertFrom) {
+				var v = c.Value (value);
+				if (v != null)
+					return v;
+			}
+			return base.ConvertFrom (context, culture, value);
+		}
+
+		public override bool CanConvertTo (ITypeDescriptorContext context, Type destinationType)
+		{
+			if (convertTo.Any (c => c.Key == destinationType))
+				return true;
+			return base.CanConvertTo (context, destinationType);
+		}
+
+		public override object ConvertTo (ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+		{
+			var n = (SimpleNumber2) value;
+			foreach (var c in convertTo) {
+				var v = c.Value (n);
+				if (v != null)
+					return v;
+			}
+			return base.ConvertTo (context, culture, value, destinationType);
+		}
+	}
+
+	public class SimpleNumber2Math : Math<SimpleNumber2> {
+		public override SimpleNumber2 Add (SimpleNumber2 x, SimpleNumber2 y)
+		{
+			return new SimpleNumber2 (x.Value + y.Value);
+		}
+
+		public override SimpleNumber2 Subtract (SimpleNumber2 x, SimpleNumber2 y)
+		{
+			return new SimpleNumber2 (x.Value - y.Value);
+		}
+
+		public override SimpleNumber2 Multiply (SimpleNumber2 x, SimpleNumber2 y)
+		{
+			return new SimpleNumber2 (x.Value * y.Value);
+		}
+
+		public override SimpleNumber2 QuotientRemainder (SimpleNumber2 x, SimpleNumber2 y, out SimpleNumber2 remainder)
+		{
+			remainder = new SimpleNumber2 (x.Value % y.Value);
+			return new SimpleNumber2 (x.Value / y.Value);
+		}
+	}
+
+	[TestFixture]
+	public class SimpleNumber2MathTests : MathContract<SimpleNumber2> {
+
+		static SimpleNumber2MathTests ()
+		{
+			Math<SimpleNumber2>.SetDefault (new SimpleNumber2Math ());
+		}
+	}
+
+	public class SimpleNumber3 : SimpleNumber, IComparable<SimpleNumber3>, IEquatable<SimpleNumber3>, IConvertible
+	{
+		public SimpleNumber3 (int value)
+			: base (value)
+		{
+		}
+
+		public int CompareTo (SimpleNumber3 x)
+		{
+			return base.CompareTo (x);
+		}
+
+		public bool Equals (SimpleNumber3 other)
+		{
+			return base.Equals (other);
+		}
+
+		TypeCode IConvertible.GetTypeCode ()
+			{return TypeCode.Object;}
+		bool IConvertible.ToBoolean (IFormatProvider p)
+			{return ((IConvertible) Value).ToBoolean (p);}
+		byte IConvertible.ToByte (IFormatProvider p)
+			{return ((IConvertible) Value).ToByte (p);}
+		char IConvertible.ToChar (IFormatProvider p)
+			{return ((IConvertible) Value).ToChar (p);}
+		DateTime IConvertible.ToDateTime (IFormatProvider p)
+			{return ((IConvertible) Value).ToDateTime (p);}
+		decimal IConvertible.ToDecimal (IFormatProvider p)
+			{return ((IConvertible) Value).ToDecimal (p);}
+		double IConvertible.ToDouble (IFormatProvider p)
+			{return ((IConvertible) Value).ToDouble (p);}
+		short IConvertible.ToInt16 (IFormatProvider p)
+			{return ((IConvertible) Value).ToInt16 (p);}
+		int IConvertible.ToInt32 (IFormatProvider p)
+			{return ((IConvertible) Value).ToInt32 (p);}
+		long IConvertible.ToInt64 (IFormatProvider p)
+			{return ((IConvertible) Value).ToInt64 (p);}
+		sbyte IConvertible.ToSByte (IFormatProvider p)
+			{return ((IConvertible) Value).ToSByte (p);}
+		float IConvertible.ToSingle (IFormatProvider p)
+			{return ((IConvertible) Value).ToSingle (p);}
+		string IConvertible.ToString (IFormatProvider p)
+			{return ((IConvertible) Value).ToString (p);}
+		object IConvertible.ToType (Type to, IFormatProvider p)
+			{return ((IConvertible) Value).ToType (to, p);}
+		ushort IConvertible.ToUInt16 (IFormatProvider p)
+			{return ((IConvertible) Value).ToUInt16 (p);}
+		uint IConvertible.ToUInt32 (IFormatProvider p)
+			{return ((IConvertible) Value).ToUInt32 (p);}
+		ulong IConvertible.ToUInt64 (IFormatProvider p)
+			{return ((IConvertible) Value).ToUInt64 (p);}
+	}
+
+	public class SimpleNumber3Math : Math<SimpleNumber3> {
+
+		public override SimpleNumber3 FromInt32 (int value)
+		{
+			return new SimpleNumber3 (value);
+		}
+
+		public override SimpleNumber3 Add (SimpleNumber3 x, SimpleNumber3 y)
+		{
+			return new SimpleNumber3 (x.Value + y.Value);
+		}
+
+		public override SimpleNumber3 Subtract (SimpleNumber3 x, SimpleNumber3 y)
+		{
+			return new SimpleNumber3 (x.Value - y.Value);
+		}
+
+		public override SimpleNumber3 Multiply (SimpleNumber3 x, SimpleNumber3 y)
+		{
+			return new SimpleNumber3 (x.Value * y.Value);
+		}
+
+		public override SimpleNumber3 QuotientRemainder (SimpleNumber3 x, SimpleNumber3 y, out SimpleNumber3 remainder)
+		{
+			remainder = new SimpleNumber3 (x.Value % y.Value);
+			return new SimpleNumber3 (x.Value / y.Value);
+		}
+	}
+
+	[TestFixture]
+	public class SimpleNumber3MathTests : MathContract<SimpleNumber3> {
+
+		static SimpleNumber3MathTests ()
+		{
+			Math<SimpleNumber3>.SetDefault (new SimpleNumber3Math ());
+		}
 	}
 }
