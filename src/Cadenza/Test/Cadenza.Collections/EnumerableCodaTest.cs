511d2370591a8c2d045835143ccd76967f07b67c
Jonathan Pryor <jonpryor@vt.edu>
2011-09-03 10:40:20 -0400

    [docs] Document EnumerableCoda.OrderByNatural().

--- src/Cadenza/Test/Cadenza.Collections/EnumerableCodaTest.cs
+++ src/Cadenza/Test/Cadenza.Collections/EnumerableCodaTest.cs
@@ -719,6 +719,7 @@ namespace Cadenza.Collections.Tests {
 		[Test]
 		public void OrderByNatural ()
 		{
+			#region OrderByNatural
 			string[] expected = {
 				"a.1.b.2.c.3.d.4.e.5.f.6.g.7.h.8.i.9.j.10.k.11",
 				"a.1.b.2.c.3.d.4.e.5.f.6.g.7.h.8.i.9.j.10.k.12",
@@ -755,6 +756,7 @@ namespace Cadenza.Collections.Tests {
 			}.OrderByNatural (s => s);
 
 			AssertAreSame (expected, actual);
+			#endregion
 		}
 
 		[Test]
